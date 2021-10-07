c5304090957499d711b93bc5c06707582c8004c0
Nargiza <n.kasymova2021@gmail.com>
2021-10-07 14:11:01 +0600

    generics in functions

--- dist/generic.js
+++ dist/generic.js
@@ -1,2 +1,58 @@
 "use strict";
+function mergeObjects(a, b) {
+    return Object.assign({}, a, b);
+}
+const merged = mergeObjects({ name: "Nargo" }, { job: "Deva" });
+console.log(merged);
+console.log(merged.name);
+const merged2 = mergeObjects({ model: "kjfd" }, { kind: "good" });
+function withCount(value) {
+    return {
+        value,
+        count: `В этом объекте ${value.length} символов`
+    };
+}
+function getObjectValue(obj, key) {
+    return obj[key];
+}
+const person = {
+    name: 'Asel',
+    job: 'Accountant',
+    age: 23
+};
+class Collection {
+    constructor(_items = []) {
+        this._items = _items;
+    }
+    add(item) {
+        this._items.push(item);
+    }
+    remove(item) {
+        this._items = this._items.filter(i => i !== item);
+    }
+    get items() {
+        return this._items;
+    }
+}
+const strings = new Collection(['I', 'am', 'strings']);
+strings.add('!');
+strings.remove('am');
+const numbers = new Collection([1, 2, 3, 4]);
+numbers.add(11);
+numbers.remove(1);
+function createAndValidateCar(model, year) {
+    const car = {};
+    if (model.length > 3) {
+        car.model = model;
+    }
+    if (year > 2000) {
+        car.year = year;
+    }
+    return car;
+}
+const cars = ['mazda', 'nexia'];
+const ford = {
+    model: 'ford',
+    year: 2021
+};
 //# sourceMappingURL=generic.js.map
--- src/generic.ts
+++ src/generic.ts
@@ -48,11 +48,73 @@ function withCount <T extends ILength>(value: T): {value: T, count: string} {
 // console.log(withCount(20))
 // console.log(withCount({length: 20}))
 
-function getObjectValue(<T extends object, R extends keyof T>(obj: T, key: R) {
+function getObjectValue<T extends object, R extends keyof T>(obj: T, key: R) {
     return obj[key]
 }
 const person = {
-    name: 'Nargo',
-    job: 'Deva'
+    name: 'Asel',
+    job: 'Accountant',
+    age: 23
 }
-console.log(getObjectValue(person, 'name'))
+// //person.age: number = 23
+// console.log(getObjectValue(person, 'name'))
+// console.log(getObjectValue(person, 'job'))
+// console.log(getObjectValue(person, "age"))
+
+class Collection<T> {
+    
+    constructor(private _items: T[] = []) {}
+    add(item: T) {
+        this._items.push(item)
+    }
+
+    remove(item: T) {
+        this._items = this._items.filter(i => i !== item)
+    }
+
+    get items(): T[] {
+        return this._items
+    }
+    
+}
+
+const strings = new Collection(['I', 'am', 'strings'])
+strings.add('!')
+strings.remove('am')
+// console.log(strings.items)
+
+const numbers = new Collection([1, 2, 3, 4])
+numbers.add(11)
+numbers.remove(1)
+// console.log(numbers.items)
+
+// const objs = new Collection([{a: 'ya'}, {b: 'a'}])
+// objs.remove({b: 'a'})
+// console.log(objs.items)
+
+interface Car {
+    model: string,
+    year: number
+}
+
+function createAndValidateCar(model: string, year: number): Car {
+    const car: Partial <Car> = {}
+    if(model.length > 3) {
+        car.model = model
+    }
+    if(year > 2000) {
+        car.year = year
+    }
+
+    return car as Car
+}
+
+const cars: Readonly< Array<string>> = ['mazda', 'nexia']
+// cars.shift()
+// cars[1]
+
+const ford: Readonly<Car> = {
+    model: 'ford',
+    year: 2021
+}
+// ford.model = "ferrari"
